1b14e6d611033595bd06aca8479b3c9bccb4fe35
porcey <827287515@qq.com>
2022-10-03 08:16:46 +0800

    项目配置及路由初始化

---
+++ src/views/login/index.vue
@@ -0,0 +1,142 @@
+<template>
+  <div class="login-container">
+    <el-form :model="form" class="login-form">
+      <div class="title-container">
+        <h3 class="title">用户登录</h3>
+      </div>
+      <el-icon :size="20" class="svg-container">
+        <Edit />
+      </el-icon>
+      <el-form-item>
+        <el-input v-model="form.name" />
+      </el-form-item>
+      <el-icon :size="20" class="svg-container">
+        <Edit />
+      </el-icon>
+      <el-form-item>
+        <el-input v-model="form.name" />
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+import { reactive } from '@vue/reactivity'
+import { Edit } from '@element-plus/icons-vue'
+export default {
+  name: 'Login',
+  components: { Edit },
+  setup() {
+    const form = reactive({
+      name: ''
+    })
+    return {
+      form
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+$bg: #2d3a4b;
+$dark_gray: #889aa4;
+$light_gray: #eee;
+$cursor: #fff;
+
+.login-container {
+  height: 100%;
+  width: 100%;
+  background-color: $bg;
+  overflow: hidden;
+
+  .login-form {
+    position: relative;
+    width: 520px;
+    max-width: 100%;
+    padding: 160px 35px 0;
+    margin: 0 auto;
+    overflow: hidden;
+
+    :deep .el-form-item {
+      border: 1px solid rgba(255, 255, 255, 0.1);
+      background: rgba(0, 0, 0, 0.1);
+      border-radius: 5px;
+      color: #454545;
+    }
+
+    :deep .el-input {
+      display: inline-block;
+      height: 47px;
+      width: 85%;
+
+      input {
+        background: transparent;
+        border: 0px;
+        -webkit-appearance: none;
+        border-radius: 0px;
+        padding: 12px 5px 12px 15px;
+        color: $light_gray;
+        height: 47px;
+        caret-color: $cursor;
+      }
+    }
+    .login-button {
+      width: 100%;
+      box-sizing: border-box;
+    }
+  }
+
+  .tips {
+    font-size: 16px;
+    line-height: 28px;
+    color: #fff;
+    margin-bottom: 10px;
+
+    span {
+      &:first-of-type {
+        margin-right: 16px;
+      }
+    }
+  }
+
+  .svg-container {
+    padding: 6px 5px 6px 15px;
+    color: $dark_gray;
+    vertical-align: middle;
+    display: inline-block;
+  }
+
+  .title-container {
+    position: relative;
+
+    .title {
+      font-size: 26px;
+      color: $light_gray;
+      margin: 0px auto 40px auto;
+      text-align: center;
+      font-weight: bold;
+    }
+
+    :deep .lang-select {
+      position: absolute;
+      top: 4px;
+      right: 0;
+      background-color: white;
+      font-size: 22px;
+      padding: 4px;
+      border-radius: 4px;
+      cursor: pointer;
+    }
+  }
+
+  .show-pwd {
+    // position: absolute;
+    // right: 10px;
+    // top: 7px;
+    font-size: 16px;
+    color: $dark_gray;
+    cursor: pointer;
+    user-select: none;
+  }
+}
+</style>
